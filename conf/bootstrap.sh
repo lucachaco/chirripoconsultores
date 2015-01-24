8156d12ba2e09db54dd07b0e46b1b9dbb006a375
lucachaco <luiscarlosch@gmail.com>
2015-01-24 13:31:37 -0600

    Init project.

---
+++ conf/bootstrap.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+apt-get update
+
+export LANGUAGE=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+locale-gen en_US.UTF-8
+dpkg-reconfigure locales
+
+apt-get install -y git
+
+#install node -> https://github.com/joyent/node/wiki/installing-node.js-via-package-manager#debian-and-ubuntu-based-linux-distributions
+curl -sL https://deb.nodesource.com/setup | sudo bash -
+sudo apt-get install -y nodejs
+
+#install sails
+npm -g install sails
